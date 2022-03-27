26e650347c6d219f931980fdee1fda22567cc03b
RodrigoItachi <sousarodrigo56@outlook.com>
2022-03-27 16:30:38 -0400

    User entity and resource

---
+++ src/main/java/com/hawkandwolf/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.hawkandwolf.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hawkandwolf.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail", "(99)99999-9999", "12345678");
+		return ResponseEntity.ok().body(u);
+	}
+}
