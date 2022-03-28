17157eaa932677af6dfa4d7ad3ad07d2332286c7
RodrigoItachi <sousarodrigo56@outlook.com>
2022-03-28 01:14:34 -0400

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/hawkandwolf/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.hawkandwolf.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hawkandwolf.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
